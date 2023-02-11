6727b2b2ef15cd1ce057b9ae228207ddbf9b9eb8
aakross <royal9619@gmail.com>
2023-02-11 17:32:36 -0600

    pull request seccion1

--- src/Components/Seccion1.jsx
+++ src/Components/Seccion1.jsx
@@ -8,6 +8,7 @@ function seccion1() {
                 <div className="text-black text-center text-5xl ">What items to store?</div>
                 <div className="text-center mt-3">Select witch items you wish to sotre before moving to</div>
                 <div className="text-center">your new home. We´ll keep´em safe!</div>
+                
             </div>
         </>
     )

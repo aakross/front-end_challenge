654611abf75c4f783d835e3ce142c9df8f2fb72c
aakross <royal9619@gmail.com>
2023-02-11 17:25:53 -0600

    seccion1 agregada

---
+++ src/Components/Seccion1.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function seccion1() {
+    return (
+        <>
+
+            <div className="mt-20 mb-10 ">
+                <div className="text-black text-center text-5xl ">What items to store?</div>
+                <div className="text-center mt-3">Select witch items you wish to sotre before moving to</div>
+                <div className="text-center">your new home. We´ll keep´em safe!</div>
+            </div>
+        </>
+    )
+}
+export default seccion1;
